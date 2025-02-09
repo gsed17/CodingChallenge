fe94a10c7565bcd9071bd414699d3918ab391c04
Garron Seddon <garronseddon@icloud.com>
2025-02-09 15:01:08 -0500

    Task 1 - Bruce Wayne's Profile

---
+++ codingchallenge5/cc_5.js
@@ -0,0 +1,10 @@
+// Task 1: Object Properties
+const batman = {
+    name: "Bruce Wayne",
+    age: 41,
+    email: "batman@gotham.com"
+};
+
+console.log(`Name: ${batman.name}`);
+console.log(`Age: ${batman.age}`);
+console.log(`Email: ${batman.email}`);
