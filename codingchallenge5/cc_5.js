9533b1d7bd98cb64a015d4f05c35a37c922343ec
Garron Seddon <garronseddon@icloud.com>
2025-02-09 15:01:40 -0500

    Task 2 - Joker’s Heist Plan

--- codingchallenge5/cc_5.js
+++ codingchallenge5/cc_5.js
@@ -8,3 +8,14 @@ const batman = {
 console.log(`Name: ${batman.name}`);
 console.log(`Age: ${batman.age}`);
 console.log(`Email: ${batman.email}`);
+// Task 2: Object Methods
+const heist = {
+    orderId: 56789,
+    totalAmount: 5000000,
+    status: "In Progress",
+    displayHeistDetails: function() {
+        console.log(`Heist ID: ${this.orderId}, Loot: $${this.totalAmount}, Status: ${this.status}`);
+    }
+};
+
+heist.displayHeistDetails();
